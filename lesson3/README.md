1ae31862b81cece3a9cf1083f86366a1c986836d
karlsheng99 <karlsheng99@gmail.com>
2021-03-02 14:39:53 -0500

    Modified sun.py

---
+++ lesson3/README.md
@@ -0,0 +1,7 @@
+# Lesson 3
+Modified sun.py. 
+* It is able to display dawn, sunrise, noon, sunset, and dusk times of a city that is not in the astral.geocoder database.
+* Users are able to manually add the city to the database by entering the timezone.
+* It generates the geographic location of the city by using Nominatim from geopy.geocoders library. (example is given in coordinates.py)
+
+![demo](sun.png)
---
+++ lesson3/sun.py
@@ -0,0 +1,34 @@
+import sys, pytz
+from datetime import date
+from astral.geocoder import database, lookup
+from astral.sun import sun
+from astral import LocationInfo
+from geopy.geocoders import Nominatim
+
+city_name = sys.argv[1]
+
+try:
+    city = lookup(city_name, database())
+    print('Information for %s/%s\n' % (city.name, city.region))
+except:
+    print("This city is not available!")
+    while True:
+        ans = input("Add it to the database?(y/n) ")
+        if ans == "y": break
+        elif ans == "n": quit()
+    #region = input("Enter the region: ")
+    timezone = input("Enter the timezone: ")
+    geolocator = Nominatim(user_agent="iot-application")
+    location = geolocator.geocode(city_name, language="en")
+    city = LocationInfo(city_name, location.address, timezone, location.latitude, location.longitude)
+    print('Information for %s\n' % (city.region))
+
+timezone = city.timezone
+print('Timezone: %s' % timezone)
+print('Latitude: %.02f; Longitude: %.02f\n' % (city.latitude, city.longitude))
+s = sun(city.observer, date=date.today(), tzinfo=pytz.timezone(timezone))
+print('Dawn:    %s' % str(s['dawn']))
+print('Sunrise: %s' % str(s['sunrise']))
+print('Noon:    %s' % str(s['noon']))
+print('Sunset:  %s' % str(s['sunset']))
+print('Dusk:    %s' % str(s['dusk']))
